2895e52eb78c845feaab02ccdb3685db94840605
DieumegardGregory <dieumegard.gregory@gmail.com>
2023-02-28 09:47:10 +0100

    un test de plus

---
+++ client/src/components/Footer/Footer .jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <>
+            <p>Politique de confidentialité</p>
+            <p>About us</p>
+        </>
+    );
+};
+
+export default Footer;
