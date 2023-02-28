84e2540ddb2a4acb547e64d8285178f4fa38d0e1
DieumegardGregory <dieumegard.gregory@gmail.com>
2023-02-28 09:42:28 +0100

    nouveau test

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,15 @@
-import Header from './components/Header/Header';
+import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-        <Header />
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+      </header>
     </div>
   );
 }
