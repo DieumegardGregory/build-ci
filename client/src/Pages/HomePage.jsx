0abebdced53c8401d165e8e551128e66512e93ec
DieumegardGregory <dieumegard.gregory@gmail.com>
2023-02-28 10:07:59 +0100

    un dernier dernier pour la route

--- client/src/Pages/HomePage.jsx
+++ client/src/Pages/HomePage.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Header from '../components/Header/Header';
+import List from '../components/List/List';
 import Footer from '../components/Footer/Footer ';
 
 const HomePage = () => {
@@ -7,6 +8,7 @@ const HomePage = () => {
         <>
             <Header />
             <section>Coucou</section>
+            <List />
             <Footer />
         </>
     );
